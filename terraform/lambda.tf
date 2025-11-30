6fe92528d8886db83db919279e44dc3cc761edd9
Antonio Estela <antonioestela73@gmail.com>
2025-11-30 00:15:44 -0500

    feat: Add SES permission

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -21,6 +21,25 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_iam_role_policy" "lambda_ses_send" {
+  name = "lambda-ses-send"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ses:SendEmail",
+          "ses:SendRawEmail"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_lambda_function" "vehicle_status" {
   function_name    = "vehicle_status_function"
   handler          = "index.handler"
