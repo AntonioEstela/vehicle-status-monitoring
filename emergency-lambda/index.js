10cc54bc18922604b338e4bfb8cb08e5ba4dac4e
Antonio Estela <antonioestela73@gmail.com>
2025-12-01 16:56:02 -0500

    feat: improve workflow

--- emergency-lambda/index.js
+++ emergency-lambda/index.js
@@ -1,4 +1,42 @@
+import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
+const sesClient = new SESClient({ region: 'sa-east-1' });
+
 export const handler = async (event) => {
+  console.log('Evento recibido:', event);
+  const eventParsed = typeof event?.body === 'string' ? JSON.parse(event.body || '{}') : event?.body || {};
+
+  const { vehicle_plate, coordinates } = eventParsed;
+
+  if (!vehicle_plate || !coordinates) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Invalid request' }),
+    };
+  }
+
+  const params = {
+    Destination: { ToAddresses: ['antonioestela73@gmail.com'] },
+    Message: {
+      Body: {
+        Text: {
+          Data: `An emergency has been reported for vehicle ${vehicle_plate} at coordinates: ${JSON.stringify(
+            coordinates
+          )}`,
+        },
+      },
+      Subject: { Data: `🚨 Emergency Alert for Vehicle ${vehicle_plate} 🚨` },
+    },
+    Source: 'antonioestela73@gmail.com',
+  };
+
+  try {
+    const command = new SendEmailCommand(params);
+    const data = await sesClient.send(command);
+    console.log('Email sent!', data);
+  } catch (err) {
+    console.error('Error sending email', err);
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Processed' }),
--- vehicle-lambda/index.js
+++ vehicle-lambda/index.js
@@ -1,7 +1,7 @@
-import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
+import { SNSClient, PublishCommand, SNS } from '@aws-sdk/client-sns';
 
-// Cliente SES reutilizable entre invocaciones
-const sesClient = new SESClient({ region: 'sa-east-1' });
+const sns = new SNSClient({ region: 'sa-east-1' });
+const SNS_ARN = 'arn:aws:sns:sa-east-1:730335330651:vehicle-emergencies'; // Can be moved to env variable
 
 export const handler = async (event) => {
   const eventParsed = typeof event?.body === 'string' ? JSON.parse(event.body || '{}') : event?.body || {};
@@ -16,28 +16,12 @@ export const handler = async (event) => {
   }
 
   if (type === 'Emergency') {
-    const params = {
-      Destination: { ToAddresses: ['antonioestela73@gmail.com'] },
-      Message: {
-        Body: {
-          Text: {
-            Data: `An emergency has been reported for vehicle ${vehicle_plate} at coordinates: ${JSON.stringify(
-              coordinates
-            )}`,
-          },
-        },
-        Subject: { Data: `🚨 Emergency Alert for Vehicle ${vehicle_plate} 🚨` },
-      },
-      Source: 'antonioestela73@gmail.com',
-    };
-
-    try {
-      const command = new SendEmailCommand(params);
-      const data = await sesClient.send(command);
-      console.log('Email sent!', data);
-    } catch (err) {
-      console.error('Error sending email', err);
-    }
+    await sns.send(
+      new PublishCommand({
+        TopicArn: SNS_ARN,
+        Message: JSON.stringify({ type, vehicle_plate, coordinates }),
+      })
+    );
   }
 
   return {
